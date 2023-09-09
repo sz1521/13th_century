0e5f6ccaf374079977eb361010109ed6c90fd5ef
Sami Heikkinen <521design@gmail.com>
2023-09-09 12:50:47 +0300

    Add new engine for sfx. Add the tune.

--- src/sfx/song.js
+++ src/sfx/song.js
@@ -7,49 +7,175 @@
     // Song data
     export const song = {
       songData: [
-        {
-          // Instrument 0
+        { // Instrument 0
           i: [
-            0, // OSC1_WAVEFORM
-            0, // OSC1_VOL
-            140, // OSC1_SEMI
-            0, // OSC1_XENV
-            0, // OSC2_WAVEFORM
-            0, // OSC2_VOL
-            140, // OSC2_SEMI
-            0, // OSC2_DETUNE
-            0, // OSC2_XENV
-            255, // NOISE_VOL
-            158, // ENV_ATTACK
-            158, // ENV_SUSTAIN
-            158, // ENV_RELEASE
-            0, // ARP_CHORD
-            0, // ARP_SPEED
-            0, // LFO_WAVEFORM
-            51, // LFO_AMT
-            2, // LFO_FREQ
-            1, // LFO_FX_FREQ
-            2, // FX_FILTER
-            58, // FX_FREQ
-            239, // FX_RESONANCE
-            0, // FX_DIST
-            32, // FX_DRIVE
-            88, // FX_PAN_AMT
-            1, // FX_PAN_FREQ
-            157, // FX_DELAY_AMT
-            2, // FX_DELAY_TIME
+          1, // OSC1_WAVEFORM
+          57, // OSC1_VOL
+          128, // OSC1_SEMI
+          0, // OSC1_XENV
+          1, // OSC2_WAVEFORM
+          201, // OSC2_VOL
+          128, // OSC2_SEMI
+          34, // OSC2_DETUNE
+          0, // OSC2_XENV
+          0, // NOISE_VOL
+          5, // ENV_ATTACK
+          6, // ENV_SUSTAIN
+          58, // ENV_RELEASE
+          0, // ENV_EXP_DECAY
+          0, // ARP_CHORD
+          0, // ARP_SPEED
+          0, // LFO_WAVEFORM
+          195, // LFO_AMT
+          6, // LFO_FREQ
+          1, // LFO_FX_FREQ
+          2, // FX_FILTER
+          135, // FX_FREQ
+          0, // FX_RESONANCE
+          0, // FX_DIST
+          9, // FX_DRIVE
+          147, // FX_PAN_AMT
+          6, // FX_PAN_FREQ
+          0, // FX_DELAY_AMT
+          1 // FX_DELAY_TIME
           ],
           // Patterns
-          p: [1, 2],
+          p: [,,,2,1,2,1,2,1,2,1,2],
           // Columns
           c: [
-            { n: [147], f: [] },
-            { n: [], f: [] },
+            {n: [94,,,106,,,89,,,101,,103,123,103,111,,96,,,108,,,90,,89,,,89,106,108,99,101],
+             f: []},
+            {n: [94,,,106,,,89,,,101,,103,123,103,111,,96,,,108,,,90,,89,,,89],
+             f: []}
+          ]
+        },
+        { // Instrument 1
+          i: [
+          0, // OSC1_WAVEFORM
+          60, // OSC1_VOL
+          128, // OSC1_SEMI
+          64, // OSC1_XENV
+          0, // OSC2_WAVEFORM
+          210, // OSC2_VOL
+          128, // OSC2_SEMI
+          0, // OSC2_DETUNE
+          64, // OSC2_XENV
+          255, // NOISE_VOL
+          4, // ENV_ATTACK
+          6, // ENV_SUSTAIN
+          73, // ENV_RELEASE
+          79, // ENV_EXP_DECAY
+          0, // ARP_CHORD
+          0, // ARP_SPEED
+          0, // LFO_WAVEFORM
+          64, // LFO_AMT
+          7, // LFO_FREQ
+          1, // LFO_FX_FREQ
+          3, // FX_FILTER
+          195, // FX_FREQ
+          15, // FX_RESONANCE
+          0, // FX_DIST
+          32, // FX_DRIVE
+          20, // FX_PAN_AMT
+          0, // FX_PAN_FREQ
+          24, // FX_DELAY_AMT
+          6 // FX_DELAY_TIME
           ],
+          // Patterns
+          p: [,,,,1,1,1,1,1,1,1,1],
+          // Columns
+          c: [
+            {n: [,,135,,,,135,,,,135,,,,135,135,,,135,,,,135,,,,135,,,,135,135],
+             f: []}
+          ]
+        },
+        { // Instrument 2
+          i: [
+          0, // OSC1_WAVEFORM
+          88, // OSC1_VOL
+          116, // OSC1_SEMI
+          79, // OSC1_XENV
+          0, // OSC2_WAVEFORM
+          255, // OSC2_VOL
+          116, // OSC2_SEMI
+          0, // OSC2_DETUNE
+          83, // OSC2_XENV
+          0, // NOISE_VOL
+          4, // ENV_ATTACK
+          6, // ENV_SUSTAIN
+          69, // ENV_RELEASE
+          52, // ENV_EXP_DECAY
+          0, // ARP_CHORD
+          0, // ARP_SPEED
+          0, // LFO_WAVEFORM
+          0, // LFO_AMT
+          0, // LFO_FREQ
+          0, // LFO_FX_FREQ
+          2, // FX_FILTER
+          14, // FX_FREQ
+          0, // FX_RESONANCE
+          0, // FX_DIST
+          32, // FX_DRIVE
+          0, // FX_PAN_AMT
+          0, // FX_PAN_FREQ
+          0, // FX_DELAY_AMT
+          0 // FX_DELAY_TIME
+          ],
+          // Patterns
+          p: [,,,,,1,1,1,1,1,1,1],
+          // Columns
+          c: [
+            {n: [135,,,,135,,,,135,,,,135,,,,135,,,,135,,,,135,,,,135],
+             f: []}
+          ]
+        },
+        { // Instrument 3
+          i: [
+          0, // OSC1_WAVEFORM
+          80, // OSC1_VOL
+          141, // OSC1_SEMI
+          1, // OSC1_XENV
+          0, // OSC2_WAVEFORM
+          69, // OSC2_VOL
+          137, // OSC2_SEMI
+          12, // OSC2_DETUNE
+          1, // OSC2_XENV
+          3, // NOISE_VOL
+          0, // ENV_ATTACK
+          48, // ENV_SUSTAIN
+          60, // ENV_RELEASE
+          4, // ENV_EXP_DECAY
+          114, // ARP_CHORD
+          1, // ARP_SPEED
+          0, // LFO_WAVEFORM
+          251, // LFO_AMT
+          6, // LFO_FREQ
+          1, // LFO_FX_FREQ
+          2, // FX_FILTER
+          255, // FX_FREQ
+          0, // FX_RESONANCE
+          0, // FX_DIST
+          32, // FX_DRIVE
+          47, // FX_PAN_AMT
+          3, // FX_PAN_FREQ
+          157, // FX_DELAY_AMT
+          2 // FX_DELAY_TIME
+          ],
+          // Patterns
+          p: [1,2,1,2,1,2,1,2,3,3,3,3],
+          // Columns
+          c: [
+            {n: [130,,,142,,,125,,,137,,139,,,147,,132,,,144,,,126,,125,,,125,,,,137],
+             f: []},
+            {n: [130,,,142,,,125,,,137,,139,,,147,,132,,,144,,,,,149,,,125,,,,137],
+             f: []},
+            {n: [130,,,,,,,,,,,,,,,,132],
+             f: []}
+          ]
         },
       ],
-      rowLen: 5513, // In sample lengths
-      patternLen: 32, // Rows per pattern
-      endPattern: 1, // End pattern
-      numChannels: 1, // Number of channels
+      rowLen: 11025,   // In sample lengths
+      patternLen: 32,  // Rows per pattern
+      endPattern: 11,  // End pattern
+      numChannels: 4  // Number of channels
     };
