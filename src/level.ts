8dbed7c7cf1e8e14a1ccf8b5d90af8097ad08978
Sami Heikkinen <521design@gmail.com>
2023-09-11 22:40:07 +0300

    Fix fill size of light effect

--- src/level.ts
+++ src/level.ts
@@ -422,7 +422,7 @@ export class Level implements Scene {
     
         context.fillStyle = gradient;
         console.info(this.camera.zoom)
-        context.fillRect(0, 0, canvas.width / this.camera.zoom, canvas.height / this.camera.zoom);
+        context.fillRect(0, 0, 4096, 2160);
 
         context.restore();
     }
